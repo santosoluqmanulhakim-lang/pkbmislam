a794b8e2bc5fb4cf06f5d93a45c4ca3f81be8490
Santoso <santoso.luqmanulhakim@gmail.com>
2026-05-24 11:18:12 +0700

    change to pkbmsunnah.id

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://pkbmislam.id', // nama domain
+  site: 'https://pkbmsunnah.id', // nama domain
   integrations: [
     sitemap({
       filter: (page) => !page.includes('/kontribusi-pkbmii2121'),
